2a0fd0d295a42995c2b7885c164b12d54f746e21
audrhkd1220 <79095684+audrhkd1220@users.noreply.github.com>
2021-08-03 00:20:42 +0900

    bookList 도서판매 카테고리 구현 sortBookList 카테고리별 도서판매 페이지 및 기능 구현

--- 21PM_POTSTAND_final/src/main/java/com/kh/potstand/book/controller/BookController.java
+++ 21PM_POTSTAND_final/src/main/java/com/kh/potstand/book/controller/BookController.java
@@ -25,9 +25,29 @@ public class BookController {
 			@RequestParam(value ="cPage",defaultValue="1") int cPage,
 			@RequestParam(value="numPerpage",defaultValue="20") int numPerpage
 			,ModelAndView mv){
+		mv.addObject("lv1", service.selectSortLv1List());
+		mv.addObject("lv2", service.selectSortLv2List());
+		mv.addObject("lv3", service.selectSortLv3List());
+		mv.addObject("lv4", service.selectSortLv4List());
 		mv.addObject("bookList", service.selectBookList(cPage, numPerpage));
 		mv.addObject("pageBar", PageFactory.getPageBar(service.selectBookCount(), cPage, numPerpage, 5, "booklist.do"));
 		mv.setViewName("book/bookList");
+		return mv;
+	}
+	
+	@RequestMapping("/book/sortBookList.do")
+	public ModelAndView selectSortBookList(String sortNo,
+			@RequestParam(value ="cPage",defaultValue="1") int cPage,
+			@RequestParam(value="numPerpage",defaultValue="20") int numPerpage
+			,ModelAndView mv){
+		mv.addObject("lv1", service.selectSortLv1List());
+		mv.addObject("lv2", service.selectSortLv2List());
+		mv.addObject("lv3", service.selectSortLv3List());
+		mv.addObject("lv4", service.selectSortLv4List());
+		mv.addObject("bookList", service.selectSortBookList(sortNo, cPage, numPerpage));
+		mv.addObject("pageBar", 
+				PageFactory.getPageBar(service.selectSortBookCount(sortNo),cPage,numPerpage,5,"sortBookList.do","sortNo="+sortNo));
+		mv.setViewName("book/sortBookList");
 		return mv;
 	}
 }
--- 21PM_POTSTAND_final/src/main/java/com/kh/potstand/book/model/service/BookService.java
+++ 21PM_POTSTAND_final/src/main/java/com/kh/potstand/book/model/service/BookService.java
@@ -2,12 +2,21 @@ package com.kh.potstand.book.model.service;
 
 import java.util.List;
 
-import org.apache.ibatis.session.SqlSession;
-
 import com.kh.potstand.book.model.vo.Book;
+import com.kh.potstand.book.model.vo.Sort;
 
 public interface BookService {
 
 	public List<Book> selectBookList(int cPage, int numPerpage);
 	public int selectBookCount();
+	
+	//sort LV1~LV4
+	List<Sort> selectSortLv1List();
+	List<Sort> selectSortLv2List();
+	List<Sort> selectSortLv3List();
+	List<Sort> selectSortLv4List();
+	
+	//카테고리 선택 책리스트
+	List<Book> selectSortBookList(String sortNo, int cPage, int numPerpage);
+	int selectSortBookCount(String sortNo);
 }
