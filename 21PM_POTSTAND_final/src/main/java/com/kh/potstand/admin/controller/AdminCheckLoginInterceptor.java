c9397e2a9c95493f14e7e2400970be02271bd057
yoodonghyun96 <jaja5280@naver.com>
2021-08-03 16:15:23 +0900

    admin 인터셉터 bean 생성 및 클래스 생성

---
+++ 21PM_POTSTAND_final/src/main/java/com/kh/potstand/admin/controller/AdminCheckLoginInterceptor.java
@@ -0,0 +1,32 @@
+package com.kh.potstand.admin.controller;
+
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+import org.springframework.web.servlet.handler.HandlerInterceptorAdapter;
+
+import com.kh.potstand.member.model.vo.Member;
+
+
+public class AdminCheckLoginInterceptor extends HandlerInterceptorAdapter{@Override
+	public boolean preHandle(HttpServletRequest request, HttpServletResponse response, Object handler)
+			throws Exception {
+		Member m = (Member)request.getSession().getAttribute("loginMember");
+		if(m==null) {
+			request.setAttribute("msg", "로그인 후 이용해 주세요.");
+			request.setAttribute("loc", "/");
+			request.getRequestDispatcher("/WEB-INF/views/common/msg.jsp").forward(request, response);
+			return false;
+		}else if(!m.getMemberSort().equals("Y")){
+			request.setAttribute("msg", "관리자만 접근이 가능합니다.");
+			request.setAttribute("loc", "/");
+			request.getRequestDispatcher("/WEB-INF/views/common/msg.jsp").forward(request, response);
+			
+			return false;
+		}else {
+			return true;
+		}
+	}{
+	}
+}
+
